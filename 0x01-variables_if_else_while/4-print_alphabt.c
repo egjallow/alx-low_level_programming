4be2992b62dc35bd38de3c01d2ac49dc322b3db7
egjallow <egjallow7@gmail.com>
2023-08-11 08:26:44 -0700

    print alphabet except e&q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints alphabets except q,e
+ * Return: 0
+ */
+int main(void)
+{
+	char alpa = 'a';
+
+	while (alpa <= 'z')
+	{
+		if (alpa != 'e' || alpa != 'q')
+			putchar(alpa);
+			alpa++;
+	}
+	putchar(10);
+	return (0);
+}
