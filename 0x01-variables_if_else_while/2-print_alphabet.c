31e47517c0d198120d0b59b5c55b36ba1ec8add5
egjallow <egjallow7@gmail.com>
2023-08-11 07:36:08 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
