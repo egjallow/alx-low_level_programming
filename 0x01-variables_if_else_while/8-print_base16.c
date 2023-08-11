c03016a75e563e6ec6016505beb3f409cd03c82f
egjallow <egjallow7@gmail.com>
2023-08-11 10:01:48 -0700

    print hex| '

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints Hex base 16
+ * Return: 0
+ */
+int main(void)
+{
+        int a, n;
+
+        a = 97;
+        n = 48;
+        while (n <= 57)
+        {
+          putchar(n);
+          n ++;
+        }
+        while (a <= 102)
+        {
+                putchar(a);
+                a++;
+        }
+        putchar(10);
+        return (0);
+}
+
