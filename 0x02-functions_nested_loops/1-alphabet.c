575e9586fac17092fc6a67227a31d8ec0b3332fd
egjallow <egjallow7@gmial.com>
2023-08-16 00:41:54 +0000

    aphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always return (0)
+ * Description:It prints alphabet a-z
+ */
+int main(void)
+{
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	i = 0;
+	while (i < 26)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
