40c9aec29c83463a9e9f2a104abb82d7f85c6ea2
egjallow <egjallow7@gmail.com>
2023-08-24 09:25:19 -0700

    change format

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,14 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
