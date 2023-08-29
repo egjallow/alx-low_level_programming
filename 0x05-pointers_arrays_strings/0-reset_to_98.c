4444557f1b99ffcf30105185569771c1d7806baf
egjallow <egjallow7@gmail.com>
2023-08-29 04:47:38 -0700

    change the address value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - is a function to reset the number
+ * @n: take an int as an arg
+ * Return: it return zero if successful
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
