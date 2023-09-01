731b25f862906fca43b138528c7522e05cf1f704
egjallow <egjallow7@gmail.com>
2023-09-01 09:35:37 -0700

    changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
  * main - Entry point of the program
- * @argc: totall value of argv
- * @argv: is a array of string
- * Return: it return an int ant prints a add
- * of 2 num pass as  arg
+ * @argc: total number of arguments
+ * @argv: array of strings representing arguments
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
-	if (argc < 3)
+	if (argc == 1)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%d\n", 0);
 	}
 	else
 	{
-		for (i = 0; i < argc; i++)
-			sum = sum + atoi(argv[i]);
+		for (i = 1; i < argc; i++)
+		{
+			j = 0;
+
+			while (argv[i][j])
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				j++;
+			}
+
+			sum += atoi(argv[i]);
+		}
 
 		printf("%d\n", sum);
-		return (0);
 	}
 
+	return (0);
 }
+
