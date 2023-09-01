1748ebf7e446268b60a47a42bd655c94330e1c0e
egjallow <egjallow7@gmail.com>
2023-09-01 07:55:10 -0700

    print the argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	if(argc > 1)
-		printf("%d\n", (argc -1));
+	if (argc > 1)
+		printf("%d\n", (argc - 1));
 	else
 		printf("%d\n", 0);
 	return (0);
