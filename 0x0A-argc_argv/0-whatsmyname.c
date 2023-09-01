cbb9e7399bc75eddf791c272bcb4eaf08f0149fb
egjallow <egjallow7@gmail.com>
2023-09-01 06:15:30 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: totall value of argv
+ * @argv: is a array of string
+ * Return: it return an int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
