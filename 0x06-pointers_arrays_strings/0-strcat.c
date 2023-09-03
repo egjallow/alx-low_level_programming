8e6e3447a5d1adc46b9f37bbe1222843305cdb51
egjallow <egjallow7@gmial.com>
2023-09-03 20:52:50 +0000

    string concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,4 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *return_string = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (return_string);
 }
