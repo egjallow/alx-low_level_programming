8e3b4f65c6c9d0e11f10744464ba751748a852b0
egjallow <egjallow7@gmail.com>
2023-09-06 10:32:01 -0500

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - Is a string that concat
+ * @dest: Teake in pointer of dest
+ * @src: Takes in src pointer
+ * @n: takes in int as an arg
+ * Return: it returns a pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
