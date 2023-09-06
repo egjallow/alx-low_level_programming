0f490a567f8f4b595a1a59a47fffb403a854a133
egjallow <egjallow7@gmail.com>
2023-09-06 11:10:47 -0500

    compare "

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - Is a string that concat
+ * @s1: Teake in pointer of dest
+ * @s2: Takes in src pointer
+ * Return: it returns a pointer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 && s2; ++s1, ++s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		
+	}
+	if (*s1)
+			return (1);
+	if (*s2)
+		return (-1);
+	return (0);
+}
