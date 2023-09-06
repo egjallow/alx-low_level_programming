57220449b7bc62769423bf343291f1ac1e66b5af
egjallow <egjallow7@gmail.com>
2023-09-06 11:13:36 -0500

    compare "

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,10 @@ int _strcmp(char *s1, char *s2)
 	for (; *s1 && s2; ++s1, ++s2)
 	{
 		if (*s1 != *s2)
-		{
 			return (*s1 - *s2);
-		}
-		
 	}
 	if (*s1)
-			return (1);
+		return (1);
 	if (*s2)
 		return (-1);
 	return (0);
