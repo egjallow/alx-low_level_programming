61dcb15cdab843b5b93afa705adcc87ef8cb54cf
egjallow10 <egjallow10@gmail.com>
2023-09-12 02:40:46 +0000

    struct  for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct my_struct - Short description
+ * @name: name of the dog
+ * @age: age in float
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
