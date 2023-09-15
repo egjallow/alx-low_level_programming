5db1ad3e1002229a9de479b505f0fdbcbbaf6964
egjallow10 <egjallow10@gmail.com>
2023-09-15 02:45:39 +0000

    add files and create fun

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum them all
+ * @n, takes in int
+ * Returns the sum of thr list
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_list;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(my_list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(my_list, int);
+	}
+	va_end(my_list);
+	return (sum);
+}
