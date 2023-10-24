361d428b1bde2a39213790c4510592941af29189
egjallow10 <egjallow10@gmail.com>
2023-10-24 13:00:37 +0000

    number of string in accept

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
+/**
+ * _strpbrk - function to retun the occurrence of char
+ * @s: the source string
+ * @accept: search_string
+ *
+ * Return: the occurrence of the string
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
-    int i, k;
+	int i, k;
 
-    for (i = 0; *(s + i) != '\0'; i++)
-    {
-        for (k = 0; *(accept + k) != '\0'; k++)
-        {
-            if (*(s + i) == *(accept + k))
-                return (s + i);
-        }
-    }
-    return (NULL);
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (k = 0; *(accept + k) != '\0'; k++)
+		{
+			if (*(s + i) == *(accept + k))
+				return (s + i);
+		}
+	}
+	return (NULL);
 }
