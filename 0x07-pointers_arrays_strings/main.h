64a60f2f0f4baa3a2d811768feb75fce9c84a13d
egjallow10 <egjallow10@gmail.com>
2023-10-24 16:21:21 +0000

    needle in a haystack

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stddef.h>
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
