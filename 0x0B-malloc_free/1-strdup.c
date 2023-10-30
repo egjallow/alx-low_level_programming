4ea1aa1f1fac3fb2f551d781d1ecaaa759a8b6c5
egjallow10 <egjallow10@gmail.com>
2023-10-30 14:55:25 +0000

    copy a strong

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - a function that copy a string
+ * @str: the string to copy
+ * Return:a copy strin(ponter characters)
+ */
+char *_strdup(char *str)
+{
+	char *copy_str;
+	int len = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	copy_str = malloc(len * sizeof(char) + 1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		copy_str[i] = str[i];
+	}
+	return (copy_str);
+}
