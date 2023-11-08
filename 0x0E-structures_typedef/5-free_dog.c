e3f494ebc239fb1d736400d592ac779e04fac3f2
egjallow10 <egjallow10@gmail.com>
2023-11-08 02:45:05 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog
+ *
+ * @d: pointer to dog to free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
